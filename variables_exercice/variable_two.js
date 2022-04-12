d14a555b254d35f2aa41b2f4b08058e1efca57ec
Brillant01 <tataraybrillant@gmail.com>
2022-04-12 10:04:16 +0300

    SAYNA-JAVASCRIPT-EXERCICE1

---
+++ variables_exercice/variable_two.js
@@ -0,0 +1,16 @@
+let mango ;
+console.log(mango) ;   // undefined
+
+mangue = 10 ;
+console.log(mango) ;  // mango is not defined
+                        
+mango + 1 ;
+console.log(mango) ; // mango is not defined
+
+mangue += 1 ;
+console.log(mangue) ; // affiche => 11 ([mangue=10]+1)
+let banana = mangue ;
+console.log(banana) ;
+banane = banana / 2 ;
+console.log(banane) ;
+console.log(mangue) ;
